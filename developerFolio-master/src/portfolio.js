65589f00db8922f24176018fdc30b9a6f7cf8145
Nawwal Aftab Waseer <waseernawwal@gmail.com>
2025-09-17 03:51:28 -0700

    fix: image issue in achievements

--- developerFolio-master/src/portfolio.js
+++ developerFolio-master/src/portfolio.js
@@ -316,7 +316,7 @@ const achievementSection = {
       title: "Full Stack Python Intern",
       subtitle:
         "Certificate of successful completion of 10+ weeks internship as a Full Stack Python Developer.",
-      image: require("./assets/images/IMG_7684 (1).jpeg"),
+      image: require("./assets/images/innovaxel-certificate.jpeg"),
       imageAlt: "Innovaxel Internship Certificate",
       footerLink: [
         {
